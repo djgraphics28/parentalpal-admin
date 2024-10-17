2180975dc7965a875304da1f234e451701992a5a
djgraphics28 <darwin.ibay30@gmail.com>
2024-10-17 15:28:07 +0800

    updates

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Models\ParentPal;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Livewire\Features\SupportConsoleCommands\Commands\Upgrade\ChangeTestAssertionMethods;
 
 class ProfileController extends Controller
 {
@@ -20,4 +21,45 @@ class ProfileController extends Controller
             // Or return only specific fields: 'data' => $user->only(['name', 'email', 'phone']),
         ]);
     }
+
+    public function updateProfile(Request $request, $id)
+    {
+        // Get the authenticated user using the ParentPal model
+        $user = ParentPal::find($id);
+
+        // Update the user's profile with the request data
+        $user->update($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Profile updated successfully',
+            'data' => $user
+        ]);
+    }
+
+    //updatePassword
+    public function updatePassword(Request $request, $id)
+    {
+        // Get the authenticated user using the ParentPal model
+        $user = ParentPal::find($id);
+
+        // Check if the current password matches
+        if (!password_verify($request->current_password, $user->password)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Current password is incorrect'
+            ], 401);
+        }
+
+        // Update the user's password
+        $user->update([
+            'password' => bcrypt($request->new_password)
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Password updated successfully'
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('reset-password', [AuthController::class, 'resetPassword']);
 
     Route::get('/{userId}/profile', [ProfileController::class, 'getProfile'])->name('api.profile');
+    Route::PUT('/{userId}/profile', [ProfileController::class, 'updateProfile'])->name('api.update.profile');
+    Route::PUT('/{userId}/update-password', [ProfileController::class, 'updatePassword'])->name('api.update.password');
 
     Route::get('/{parentId}/children', [ChildController::class, 'index']);         // Get all children for the authenticated user
     Route::post('/{parentId}/children', [ChildController::class, 'store']);        // Create a new child
